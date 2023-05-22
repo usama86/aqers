62f1a1e5c6e8eb7b90d6a9de8cf36781bd70ff17
Asad Bhatti <bhatti.asad99@gmail.com>
2023-05-23 04:21:40 +0500

    s

--- src/components/UseCase/PropertyDetails/index.js
+++ src/components/UseCase/PropertyDetails/index.js
@@ -31,8 +31,8 @@ const PropertyDetailsComponent = ({ showMortgage, showFeaturedProperties }) => {
           containerStyles={{
             width: relative_width_size_generator(959),
             // backgroundColor: "grey",
-            height: relative_height_size_generator(580),
-            border: "1px dashed grey",
+            // height: relative_height_size_generator(580),
+            // border: "1px dashed grey",
           }}
         />
         <StackCompoent
