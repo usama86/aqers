0975d8a9980069c86cb0151ae01617f6a4277df5
Usama <usama.amjad32@gmail.com>
2023-05-30 13:59:07 +0500

    fixed responsiveness

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -45,22 +45,22 @@ export const relative_width_font_size_generator = (
   pixelValue,
   totalFrameWidthInPixels
 ) => {
-  // try {
-  //   let width;
-  //   if (!totalFrameWidthInPixels) {
-  //     width = 1920;
-  //   } else {
-  //     width = totalFrameWidthInPixels;
-  //   }
-  //   if (+pixelValue === NaN) {
-  //     return 0;
-  //   }
+  try {
+    let width;
+    if (!totalFrameWidthInPixels) {
+      width = 1920;
+    } else {
+      width = totalFrameWidthInPixels;
+    }
+    if (+pixelValue === NaN) {
+      return 0;
+    }
 
-  //   return `${(+pixelValue * 100) / width}vw`;
-  // } catch (err) {
-  //   return 0;
-  // }
-  return `${pixelValue}px`;
+    return `${(+pixelValue * 100) / width}vw`;
+  } catch (err) {
+    return 0;
+  }
+  // return `${pixelValue}px`;
 };
 
 export function capitalizeFirstLetter(str) {
